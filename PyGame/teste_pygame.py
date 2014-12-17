11f8f1310ebe8d160650ddbe29053d8e50f93c39
Marcelo <marcelomyh1011@gmail.com>
2014-12-17 11:28:43 -0300

    PyGame v0.2

--- PyGame/teste_pygame.py
+++ PyGame/teste_pygame.py
@@ -2,6 +2,11 @@ import pygame
 
 pygame.init()
 
+red = 255
+green = 255
+blue = 255
+color = (red, green, blue)
+
 gameDisplay = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Slither')
 # pygame.display.update()
@@ -17,5 +22,10 @@ while (gameRun):
         if(event.type == pygame.KEYDOWN):
             print(event)
 
+    gameDisplay.fill(color)
+
+    pygame.draw.rect(gameDisplay, (0, 0, 0), [300, 300, 10, 10])
+
+    pygame.display.update()
 pygame.quit()
 quit()
